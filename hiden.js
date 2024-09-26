2fb44787e6e7f6f8ac49039f7b2973371f2cb65b
juugaya <51192393+juugaya@users.noreply.github.com>
2024-09-26 17:24:16 +0900

    Update hiden.js

--- hiden.js
+++ hiden.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("button");
-const list = document.getElementById("list");
+const list = document.getElementById("list_1");
 
  btn.addEventListener("click",function(){
  list.classList.toggle("hidden")
