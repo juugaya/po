c89234eb451ac723be239cdb13cc539bdc9f3b81
juugaya <ogamcad@gmail.com>
2024-10-01 04:03:49 +0900

    完成2

---
+++ Changeseireki.php
@@ -0,0 +1,18 @@
+<html>
+ 	<head>
+ 			<meta http-equiv="Content-type" content="text/html;
+ 			charset=UTF-8"/>
+<title>西暦を和暦に変換するプログラム</title>
+ 	</head>
+ 	
+ 	<body>
+        >西暦を和暦に変換するプログラム(明治まで)　<br>
+ 	<form action="Changeseireki1.php" method="post">
+ 	西暦を4桁で入力してください。<br>
+ 		<input type="text" name="number" value=""> <br>
+ 	誕生月を入力してください。<br>
+ 		<input type="text" name="month" value=""> <br>
+ 	日にちを入力してください。<br>
+ 	<input type="text" name="day" value=""> <br>
+ 		<input type="submit" value="入力">
+ 		</form>
