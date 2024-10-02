95d717c87c521f7669a22ab92cd9d9fd02fb6c92
juugaya <51192393+juugaya@users.noreply.github.com>
2024-10-02 17:15:22 +0900

    Create display.js

---
+++ css/display.js
@@ -0,0 +1,17 @@
+const Sample = {
+    data() {
+        return {
+            viewFlag: true,
+        };
+    },
+    methods: {
+        on() {
+            this.viewFlag = true;
+        },
+        off() {
+            this.viewFlag = false;
+        },
+    },
+};
+
+Vue.createApp(Sample).mount("#app_3");
